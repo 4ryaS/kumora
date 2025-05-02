03950955ce2a53829f80f7795e86e79ecb907869
4ryaS <aryashrimali443@gmail.com>
2025-05-03 01:37:20 +0530

    Add dynamic subdomain-based routing with Fastify and http-proxy

--- reverse-proxy-server/server.js
+++ reverse-proxy-server/server.js
@@ -1,8 +1,41 @@
 const fastify = require('fastify')({
     logger: true
 });
+const http_proxy = require('http-proxy');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+const proxy = http_proxy.createProxyServer();
+const BASE_PATH = process.env.BASE_PATH;
 const PORT = process.env.PORT || 8000;
+
+// Catch-all route for all methods and paths
+fastify.all('*', async (request, reply) => {
+    const host_header = request.headers.host || '';
+    const hostname = host_header.split(':')[0];
+    const subdomain = hostname.split('.')[0];
+
+    const target = `${BASE_PATH}/${subdomain}`;
+
+    fastify.log.info(`Proxying to: ${target}`);
+
+    return new Promise((resolve, reject) => {
+        proxy.web(
+            request.raw,
+            reply.raw,
+            {
+                target: target,
+                changeOrigin: true
+            },
+            (err) => {
+                fastify.log.error(err);
+                reply.send(err);
+                reject(err);
+            }
+        );
+    });
+});
 
 fastify.listen({ port: PORT }, (err, address) => {
     if (err) {
